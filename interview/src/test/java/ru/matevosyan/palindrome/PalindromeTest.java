3f651612cde898f2405b838ea2c7d4ae093b6c2e
VardanMatevosyan <vardya@yandex.ru>
2019-03-05 00:35:13 +0200

    Add palindrome test "coding interview question" module

---
+++ interview/src/test/java/ru/matevosyan/palindrome/PalindromeTest.java
@@ -0,0 +1,19 @@
+package ru.matevosyan.palindrome;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PalindromeTest {
+
+    @Test
+    public void when_Then() {
+        Palindrome palindrome = new Palindrome();
+        String sentences = "SOLOS";
+        boolean actual = palindrome.check(sentences);
+
+        assertThat(actual, is(true));
+    }
+
+}
