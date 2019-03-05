95810b46627fcbfa453ed7337f46bce0a3f4cbfa
VardanMatevosyan <vardya@yandex.ru>
2019-03-05 23:17:53 +0200

    Add cycle detection in linked list test "coding interview question" module

---
+++ interview/src/test/java/ru/matevosyan/linkedList/ListCircleDetectorTest.java
@@ -0,0 +1,31 @@
+package ru.matevosyan.linkedList;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ListCircleDetectorTest {
+
+    /**
+     * Cycle in the list.
+     * 1 2 3 4 5 3 7 9.
+     *
+     */
+    @Test
+    public void whenHasCycleThenReturnTru() {
+        ListCircleDetector<Integer> list = new ListCircleDetector<>();
+
+        ListCircleDetector.Node<Integer> root = new ListCircleDetector.Node<>(1);
+        ListCircleDetector.Node<Integer> node2 = new ListCircleDetector.Node<>(2);
+        ListCircleDetector.Node<Integer> node3 = new ListCircleDetector.Node<>(3);
+
+        root.setNext(node2);
+        node2.setNext(node3);
+        node3.setNext(root);
+
+        boolean hasCircle = list.hasCircle(root);
+
+        assertThat(hasCircle, is(true));
+    }
+}
