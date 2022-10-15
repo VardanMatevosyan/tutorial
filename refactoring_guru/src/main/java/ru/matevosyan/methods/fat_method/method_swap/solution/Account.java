2e7f557352bde8fbdda495f7e93d7876dd3c3e74
VardanMatevosyan <vardanmt@gmail.com>
2022-10-15 20:34:56 +0300

    refactoring guru - refactoring fat methods with many local fields examples

---
+++ refactoring_guru/src/main/java/ru/matevosyan/methods/fat_method/method_swap/solution/Account.java
@@ -0,0 +1,13 @@
+package ru.matevosyan.methods.fat_method.method_swap.solution;
+
+class Account {
+    // ...
+    int gamma(int inputVal, int quantity, int yearToDate) {
+        return new Gamma(this, inputVal, quantity, yearToDate).compute();
+    }
+
+    public int delta() {
+        return 0;
+    }
+    // ...
+}
