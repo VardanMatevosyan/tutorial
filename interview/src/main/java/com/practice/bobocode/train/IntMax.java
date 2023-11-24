44d4b7d14cb73133ad1e8593205a345ec5ae7423
VardanMatevosyan <vardanmt@gmail.com>
2023-11-24 18:28:44 +0200

    experiment with findMax

--- interview/src/main/java/com/practice/bobocode/train/IntMax.java
+++ interview/src/main/java/com/practice/bobocode/train/IntMax.java
@@ -1,5 +1,9 @@
 package com.practice.bobocode.train;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class IntMax {
 
     public static int findMax(int[] array) {
@@ -12,8 +16,54 @@ public class IntMax {
         return result;
     }
 
+    public static int findMax(List<Integer> integers) {
+        return integers.stream()
+            .max(Integer::compareTo)
+//            .max(Comparator.comparing(Function.identity(), Integer::compareTo))
+//            .max(Comparator.comparing(Function.identity()))
+//            .max(Comparator.comparingInt(value -> value))
+//            .max(Comparator.naturalOrder())
+            .orElse(0);
+    }
+
+
+    public static int findMaxWithToIntStream(List<Integer> integers) {
+        return integers.stream()
+            .mapToInt(Integer::intValue)
+            .max()
+            .orElse(0);
+    }
+
     public static void main(String[] args) {
-        int max = findMax(new int[]{1, 6, 4});
-        System.out.println(max);
+        List<Integer> integers = getIntegers(100000);
+        int[] integersArray = getIntegersArray(100000);
+
+        long start = System.nanoTime();
+        int max = findMax(integersArray);
+        long end = ((System.nanoTime() - start) / 1_000_000);
+
+        long start2 = System.nanoTime();
+        int maxFromList = findMax(integers);
+        long end2 = ((System.nanoTime() - start2) / 1_000_000);
+
+        long start3 = System.nanoTime();
+        int maxFromListWithToIntStream = findMaxWithToIntStream(integers);
+        long end3 = ((System.nanoTime() - start3) / 1_000_000);
+
+
+        System.out.println("max is " + max + " last " + end + "ms");
+        System.out.println("maxFromList is " + maxFromList + " last " + end2 + "ms");
+        System.out.println("maxFromListWithToIntStream is " + maxFromListWithToIntStream + " last " + end3 + "ms");
+
+    }
+
+    private static int[] getIntegersArray(int capasity) {
+        return IntStream.rangeClosed(0, capasity).toArray();
+    }
+
+    private static List<Integer> getIntegers(Integer capasity) {
+        return IntStream.rangeClosed(0, capasity)
+            .boxed()
+            .collect(Collectors.toList());
     }
 }
