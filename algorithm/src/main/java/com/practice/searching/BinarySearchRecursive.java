a2a44dea7d1c787ae993ddda636a125a1270094a
VardanMatevosyan <vardanmt@gmail.com>
2023-11-27 12:28:07 +0200

    Algorithm - fix binary search

--- algorithm/src/main/java/com/practice/searching/BinarySearchRecursive.java
+++ algorithm/src/main/java/com/practice/searching/BinarySearchRecursive.java
@@ -8,26 +8,24 @@ public class BinarySearchRecursive implements Searchable<Integer, Integer> {
     public Integer search(Integer[] array, Integer element) {
         int left = 0;
         int right = array.length - 1;
-        return this.binaryRecursive(array, element, left, right);
+        return binaryRecursive(array, element, left, right);
     }
 
     private Integer binaryRecursive(Integer[] array, Integer element, int left, int right) {
-        Integer result = -1;
         int middle;
 
         if (left > right) {
-            return result;
+            return -1;
         }
 
         middle = (left + right) / 2;
 
         if (Objects.equals(array[middle], element)) {
-            result =  array[middle];
+            return array[middle];
         } else if (array[middle] > element) {
-            this.binaryRecursive(array, element, left, middle - 1);
+            return binaryRecursive(array, element, left, middle - 1);
         } else {
-            this.binaryRecursive(array, element, middle + 1, right);
+            return binaryRecursive(array, element, middle + 1, right);
         }
-        return result;
     }
 }
