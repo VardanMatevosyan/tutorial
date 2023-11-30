ef1be84802491ed5b1c9ea68567240b4c3c8c8fd
VardanMatevosyan <vardanmt@gmail.com>
2023-11-30 11:43:41 +0200

    algorithm (two pointers, string) - added two method isPalindrome with different algorithm

--- interview/src/main/java/com/practice/strings/Palindrome.java
+++ interview/src/main/java/com/practice/strings/Palindrome.java
@@ -1,48 +1,120 @@
 package com.practice.strings;
 
 /**
- * Palindrome class.
- * Check if sentences is palindrome or not.
+ * Palindrome class. Check if sentences is palindrome or not.
  */
 public class Palindrome {
 
-    public Palindrome() {
+  public Palindrome() {
+  }
+
+  /**
+   * DABBAD and CIVIC and SOLOS is the palindrome.
+   *
+   * @param sentences as String.
+   * @return true if sentences is the palindrome.
+   */
+  public boolean check1(String sentences) {
+    int pFirst = 0;
+    int pSecond = sentences.length() - 1;
+
+    if (sentences.isEmpty()) {
+      return false;
     }
 
-    /**
-     * DABBAD and CIVIC and SOLOS is the palindrome.
-     * @param sentences as String.
-     * @return true if sentences is the palindrome.
-     */
-    public boolean check(String sentences) {
-        int pFirst = 0;
-        int pSecond = sentences.length() - 1;
+    while (pFirst <= pSecond) {
+      char cFirst = sentences.charAt(pFirst++);
+      char cSecond = sentences.charAt(pSecond--);
+      if (cFirst != cSecond) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-        if (sentences.isEmpty()) {
-            return false;
-        }
+  /**
+   * This approach does not accept non-alphabetical characters.
+   *
+   * @param sentence string.
+   * @return false if string is empty, true is it is a palindrome
+   */
+  public boolean isPalindrome2(String sentence) {
+    if (sentence.isEmpty()) {
+      return false;
+    }
+    char[] chars = sentence.toCharArray();
+    for (int i = 0; i < sentence.length(); i++) {
+      if (chars[i] != chars[sentence.length() - i - 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-        while (pFirst <= pSecond) {
-            char cFirst = sentences.charAt(pFirst++);
-            char cSecond = sentences.charAt(pSecond--);
-            if (cFirst != cSecond) {
-                return false;
-            }
-        }
-        return true;
+  /**
+   * This method better it accept non-alphabetical chars and whitespaces anywhere in the string.
+   * Simple working approach
+   *
+   * @param s string.
+   * @return true if contains only whitespace and is palindrome, else false.
+   */
+  public boolean isPalindrome3(String s) {
+
+    s = prepareString(s);
+    if (s.isEmpty()) {
+      return true;
     }
 
-    public boolean isPalindrome(String sentence) {
-        if (sentence.isEmpty()) {
-            return false;
-        }
-        char[] chars = sentence.toCharArray();
-        for (int i = 0; i < sentence.length(); i++) {
-            if (chars[i] != chars[sentence.length() - i - 1]) {
-                return false;
-            }
-        }
-        return true;
+    char[] chars = s.toCharArray();
+    for (int i = 0; i < chars.length / 2; i++) {
+      if (chars[i] != chars[chars.length - i - 1]) {
+        return false;
+      }
     }
+
+    return true;
+
+  }
+
+  private String prepareString(String str) {
+    return str
+        .replaceAll("\\W|\\s|:|_", "")
+        .toLowerCase()
+        .trim();
+  }
+
+  /**
+   * The fastest approach. The best approach is using Character class. to ask if it is a digit or
+   * letter and just increase the index is much faster than even prepare all string using regex
+   * expression at ones at the beginning
+   *
+   * @param s string
+   * @return true if contains only whitespace and is palindrome, else false.
+   */
+  public boolean isPalindrome4(String s) {
+
+    if (s == null || s.isEmpty()) {
+      return true;
+    }
+
+    int start = 0, end = s.length() - 1;
+
+    while (start < end) {
+      char startChar = Character.toLowerCase(s.charAt(start));
+      char endChar = Character.toLowerCase(s.charAt(end));
+
+      if (!Character.isLetterOrDigit(startChar)) {
+        start++;
+      } else if (!Character.isLetterOrDigit(endChar)) {
+        end--;
+      } else if (startChar != endChar) {
+        return false;
+      } else {
+        start++;
+        end--;
+      }
+    }
+    return true;
+  }
 
 }
--- interview/src/test/java/com/practice/strings/PalindromeTest.java
+++ interview/src/test/java/com/practice/strings/PalindromeTest.java
@@ -7,33 +7,74 @@ import org.junit.Test;
 
 public class PalindromeTest {
 
-    @Test
-    public void when_sentencesIsPalindromeCheckMethod_thenShouldReturnTrue() {
-        Palindrome palindrome = new Palindrome();
-        String sentences = "SOLOS";
-        boolean actual = palindrome.check(sentences);
+  @Test
+  public void when_sentencesIsPalindromeCheckMethod_thenShouldReturnTrue() {
+    Palindrome palindrome = new Palindrome();
+    String sentences = "SOLOS";
+    boolean actual = palindrome.check1(sentences);
 
-        assertThat(actual, is(true));
-    }
+    assertThat(actual, is(true));
+  }
 
-    @Test
-    public void when_sentencesIsPalindrome_thenShouldReturnTrue() {
-        Palindrome palindrome = new Palindrome();
-        String sentences = "SOLOS";
+  @Test
+  public void when_sentencesIsPalindrome_thenShouldReturnTrue() {
+    Palindrome palindrome = new Palindrome();
+    String sentences = "SOLOS";
 
-        boolean actual = palindrome.isPalindrome(sentences);
+    boolean actual = palindrome.isPalindrome2(sentences);
 
-        assertThat(actual, is(true));
-    }
+    assertThat(actual, is(true));
+  }
 
-    @Test
-    public void when_sentencesIsNotPalindrome_thenShouldReturnFalse() {
-        Palindrome palindrome = new Palindrome();
-        String sentences = "SOLO";
+  @Test
+  public void when_sentencesIsNotPalindrome_thenShouldReturnFalse() {
+    Palindrome palindrome = new Palindrome();
+    String sentences = "SOLO";
 
-        boolean actual = palindrome.isPalindrome(sentences);
+    boolean actual = palindrome.isPalindrome2(sentences);
 
-        assertThat(actual, is(false));
-    }
+    assertThat(actual, is(false));
+  }
+
+  @Test
+  public void when_stringIsNotPalindromeWithNonAlphabeticalCharsUsingIsPalindrome3_thenFalse() {
+    Palindrome palindrome = new Palindrome();
+    String sentences = "SOL_O:";
+
+    boolean actual = palindrome.isPalindrome3(sentences);
+
+    assertThat(actual, is(false));
+  }
+
+  @Test
+  public void when_stringIsNotPalindromeWithNonAlphabeticalCharsUsingIsPalindrome4_thenFalse() {
+    Palindrome palindrome = new Palindrome();
+    String sentences = "S~OL_O:";
+
+    boolean actual = palindrome.isPalindrome4(sentences);
+
+    assertThat(actual, is(false));
+  }
+
+  @Test
+  public void when_stringIsPalindromeWithNonAlphabeticalCharsUsingIsPalindrome4_thenTrue() {
+    Palindrome palindrome = new Palindrome();
+    String sentences = "S~OL_O:S";
+
+    boolean actual = palindrome.isPalindrome4(sentences);
+
+    assertThat(actual, is(true));
+  }
+
+  @Test
+  public void when_stringIsPalindromeWithNonAlphabeticalCharsUsingIsPalindrome3_thenTrue() {
+    Palindrome palindrome = new Palindrome();
+    String sentences = "S~OL_O:S";
+
+    boolean actual = palindrome.isPalindrome3(sentences);
+
+    assertThat(actual, is(true));
+  }
+
 
 }
