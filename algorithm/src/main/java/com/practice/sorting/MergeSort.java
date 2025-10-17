eec6373ad2ff3e33fca701210af7b808076b285c
VardanMatevosyan <vardanmt@gmail.com>
2025-10-17 09:21:26 +0400

    refactored th MergeSort

--- algorithm/src/main/java/com/practice/sorting/MergeSort.java
+++ algorithm/src/main/java/com/practice/sorting/MergeSort.java
@@ -8,13 +8,45 @@ import java.util.Arrays;
 public class MergeSort implements Sortable<Integer> {
     @Override
     public Integer[] sort(Integer[] array) {
-        int len = array.length;
-        int middle = len / 2;
-        if (len < 2) {
+        if (array.length < 2) {
             return array;
         }
-        return merge(sort(Arrays.copyOfRange(array, 0, middle)),
-                sort(Arrays.copyOfRange(array, middle, array.length)));
+        Integer[] leftPart = sortLeftHalf(array);
+        Integer[] rightPart = sortRightHalf(array);
+        return merge(leftPart, rightPart);
+    }
+
+    /**
+     * Sort right half of the array.
+     * @param array created during each recursing call.
+     * @return sorted right half of the array.
+     */
+    private Integer[] sortRightHalf(Integer[] array) {
+        int middle = array.length / 2;
+        Integer[] rightSubArray = subArray(array, middle, array.length);
+        return sort(rightSubArray);
+    }
+
+    /**
+     * Sort left half of the array.
+     * @param array created during each recursing call.
+     * @return sorted left half of the array.
+     */
+    private Integer[] sortLeftHalf(Integer[] array) {
+        int middle = array.length / 2;
+        Integer[] leftSubArray = subArray(array, 0, middle);
+        return sort(leftSubArray);
+    }
+
+    /**
+     * Create sub array from given array.
+     * @param array created during each recursing call.
+     * @param from where to start.
+     * @param to where to end.
+     * @return sub array.
+     */
+    private static Integer[] subArray(Integer[] array, int from, int to) {
+        return Arrays.copyOfRange(array, from, to);
     }
 
     /**
@@ -26,18 +58,19 @@ public class MergeSort implements Sortable<Integer> {
     private Integer[] merge(Integer[] first, Integer[] second) {
         int size = first.length + second.length;
         Integer[] result = new Integer[size];
-        int firPointer = 0;
-        int secPointer = 0;
-        int resPointer = 0;
+        int fai = 0;
+        int sai = 0;
+        int rai = 0;
 
-        while (firPointer < first.length && secPointer < second.length) {
-            result[resPointer++] = first[firPointer] < second[secPointer] ? first[firPointer++] : second[secPointer++];
+        while (fai < first.length && sai < second.length) {
+            result[rai++] = first[fai] < second[sai] ? first[fai++] : second[sai++];
         }
-        if (firPointer < first.length) {
-            System.arraycopy(first, firPointer, result, resPointer, first.length - firPointer);
-        } else if (secPointer < second.length) {
-            System.arraycopy(second, secPointer, result, resPointer, second.length - secPointer);
+        if (fai < first.length) {
+            System.arraycopy(first, fai, result, rai, first.length - fai);
+        } else if (sai < second.length) {
+            System.arraycopy(second, sai, result, rai, second.length - sai);
         }
+
         return result;
     }
 
