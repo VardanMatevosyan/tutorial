bd572b079f5e682aee6f60078621927f65415f95
VardanMatevosyan <vardanmt@gmail.com>
2025-10-17 09:22:08 +0400

    refactored th BinarySearchIterative

--- algorithm/src/main/java/com/practice/searching/BinarySearchIterative.java
+++ algorithm/src/main/java/com/practice/searching/BinarySearchIterative.java
@@ -12,14 +12,14 @@ public class BinarySearchIterative implements Searchable<Integer, Integer> {
     private Integer binaryIterative(Integer[] array, Integer element) {
         Integer result = -1;
         int left = 0;
-        int right = decrementMiddle(array.length);
+        int right = array.length - 1;
         int middle;
         while (left <= right) {
             middle = getNewArrayMiddle(left, right);
             if (Objects.equals(array[middle], element)) {
                 return array[middle];
             } else if (array[middle] < element) {
-                left = incrmentMiddle(middle);
+                left = incrementMiddle(middle);
             } else if (array[middle] > element) {
                 right = decrementMiddle(middle);
             }
@@ -31,7 +31,7 @@ public class BinarySearchIterative implements Searchable<Integer, Integer> {
         return middle - 1;
     }
 
-    private static int incrmentMiddle(int middle) {
+    private static int incrementMiddle(int middle) {
         return middle + 1;
     }
 
